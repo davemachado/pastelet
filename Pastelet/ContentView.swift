ff711adb55de81cb9ca7f15a3afe69cf2e7afa38
Dave Machado <dave@davemachado.com>
2026-01-31 19:07:54 -0800

    Initial Commit

---
+++ Pastelet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pastelet
+//
+//  Created by Dave Machado on 1/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pastelet/PasteletApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasteletApp.swift
+//  Pastelet
+//
+//  Created by Dave Machado on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct PasteletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
